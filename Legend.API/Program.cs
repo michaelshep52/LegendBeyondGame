dba3ae2a4f97dd02abafb03eca3bbe15911f2e5b
Michael <michaelshep52@gmail.com>
2023-03-06 07:18:49 -0500

    AddedDBconnect to API and updated DBcontext

--- Legend.API/Program.cs
+++ Legend.API/Program.cs
@@ -1,11 +1,21 @@
-﻿using Legend.Data.Repositories;
+﻿
+using Legend.Data;
+using Legend.Data.Repositories;
 using Legend.Domain.Interface;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddTransient(typeof(ILegendRepository<>), typeof(LegendRepository<>));
+/*builder.Services.AddTransient<IUserRepository, IUserRepository>();
+builder.Services.AddTransient<ICharacterRepository, ICharacterRepository>();
+builder.Services.AddTransient<IMissionRepository, MissionRepository>();*/
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<LegendContext>(options => options.UseNpgsql(
+builder.Configuration.GetConnectionString("LegendGameData")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -20,9 +30,7 @@ if (app.Environment.IsDevelopment())
 }
 
 //builder.Services.AddTransient(typeof(ILegendRepository<>), typeof(LegendRepository<>));
-/*builder.Services..AddTransient<IUserRepository, UserRepository>();
-builder.Services..AddTransient<ICharacterRepository, CharacterRepository>();
-builder.Services..AddTransient<IMissionRepository, MissionRepository>();*/
+
 
 
 app.UseHttpsRedirection();
--- Legend.Data/LegendContext.cs
+++ Legend.Data/LegendContext.cs
@@ -10,9 +10,15 @@ namespace Legend.Data
 {
     public class LegendContext : DbContext
     {
+        
         public DbSet<Character> Characters { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Mission> Missions { get; set; }
+
+        public LegendContext(DbContextOptions<LegendContext> options) : base(options)
+        {
+
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
                => optionsBuilder.UseNpgsql(
